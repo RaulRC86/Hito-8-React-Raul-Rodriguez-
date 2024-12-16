9975d653ad836db34ecf2b678debd67d4222a29e
Raúl <raul.rodriguez.c.2012@gmail.com>
2024-12-16 14:44:30 -0300

    Página de perfil

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,11 +1,31 @@
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 export const Profile = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(()=> {
+    const token = localStorage.getItem("token");
+
+    if (token){
+      fetch("http://localhost:5000/api/auth/me",{
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response)=> response.json())
+      .then ((data)=> setUser(data))
+    }
+  }, [])
   return (
     <>
     <div className="perfil">
-    <p>Raúl Rodríguez Clavero</p>
-    <p>correo: raulrodriguez@gmail.com</p>
+      {user ? (
+        <p>Email: {user.email}</p>
+      ) : (
+        <p>Por favor ingresa para ver tu perfil</p>
+      )}
+
     <Link to="/" className="btn btn-light"><span className="botonP">🔒Logout</span></Link>
     </div>
     </>
